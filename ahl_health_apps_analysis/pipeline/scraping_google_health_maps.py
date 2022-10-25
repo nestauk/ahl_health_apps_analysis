3ced04a02622ba00ad0d571efc68cc6e2dae40b7
imy99 <imanmuse@NESTA-FVFFP149Q05P.broadband>
2022-10-25 10:32:23 +0100

    adding scraping file

---
+++ ahl_health_apps_analysis/pipeline/scraping_google_health_maps.py
@@ -0,0 +1,65 @@
+from ahl_health_apps_analysis.utils.scraping_google_health_maps_utils import *
+
+
+from tqdm import tqdm
+import logging
+
+general_search = [
+'health', 
+'fitness', 
+'well_being', 
+'nutrition', 
+'diet', 
+'healthy eating']
+
+apps_to_explore  = [
+'Headspace',
+'MyFitnessPal',
+'HealthTap',
+'Noom',
+'Strava',
+'Calm',
+'Fitbit',
+'Stretching Exercises - Flexibility',
+'Insight Timer',
+'Nike Running App',
+'Nike Training Club',
+'Runna',
+'Apple health app', 
+'Google fit',
+'ZOE',
+'Clue',
+'Oura',
+'WW app',
+'Polar Flow',
+'Down Dog',
+'Balance',
+'Fiton',
+'Peloton']
+
+
+
+app_ids = search_list(general_search,30) + search_list(apps_to_explore)
+
+
+
+'''Looks for similar apps many times over returned in variable named app_details_set'''
+app_details_set = set()
+for x in tqdm(app_ids[:1]):
+    logging.info(f"Getting apps related to {x}")
+    related_apps = app_snowball(x)
+    if related_apps:
+        app_details_set.update(set(related_apps))
+
+
+app_details_df = load_all_app_ids(app_details_set)
+
+app_details_df.to_csv('inputs/data/app_ids_list_5.csv')
+
+
+
+
+
+
+
+
---
+++ ahl_health_apps_analysis/utils/scraping_google_health_maps_utils.py
@@ -0,0 +1,85 @@
+
+from google_play_scraper import search
+from google_play_scraper import app
+import pandas as pd
+from tqdm import tqdm
+
+
+
+
+def search_apps(search_list, n_hits = 5):
+
+    '''Input list of apps, returns default of 5 similar apps from each list, this can be altered but the maximum n_hits is 30'''
+
+    app_ids = []
+    for search_name in search_list:
+        results = search(search_name,
+                        lang="en",  
+                        country="uk",  
+                        n_hits=n_hits 
+        )
+        for result in results:
+            app_ids.append(result['appId'])
+    return app_ids
+
+
+
+def load_all_app_ids(app_ids) -> set:
+    """
+    Loads the saved ids of all apps and returns them in a set:
+    Takes no arguments; returns a single set.
+    """
+    app_ids_df = []
+    number_errors = 0
+    for app_id in tqdm(app_ids):
+        try:
+          result = app(
+            app_id,
+            lang='en',
+            country='uk')
+          app_ids_df.append(result)
+        except:
+           number_errors += 1
+    print(f"Number of apps that errored: {number_errors} out of {len(app_ids)}")
+    return pd.DataFrame(app_ids_df)#app_ids_df[app_ids_df['genre']=='Health & Fitness']
+
+
+
+def app_snowball(seed_app_id: str, depth: int = 5, __current_depth: int = 1) -> list:
+    """
+    Retrieves ids of Play Store apps related to `seed_app_id` by calling itself recursively.
+
+    Args:
+        seed_app_id: str - the app id of the app of interest
+        depth: int, default = 5 - the depth of recursion. This will increase the number of apps interrogated (and
+        therefore the time taken for the initial call to complete) exponentially
+        __current_depth: used for recursion, should be left blank by user
+
+    Returns:
+        a list of app ids
+    """
+
+    try:
+        app_details = app(seed_app_id)
+        similar_apps = app_details["similarApps"]
+    except:
+#         logging.warning(f"{seed_app_id} could not be processed.")
+        return False
+
+    snowball = set([seed_app_id])
+    try:
+        snowball.update(similar_apps)
+    except:
+        logging.warning(f"{seed_app_id} had a problem. Maybe it has no related apps.")
+    if (__current_depth < depth) and (similar_apps is not None):
+        for this_app in similar_apps:
+            next_step_results = app_snowball(this_app, depth, (__current_depth + 1))
+            if next_step_results:
+                snowball.update(next_step_results)     
+            
+
+    return list(snowball)
+
+
+
+
