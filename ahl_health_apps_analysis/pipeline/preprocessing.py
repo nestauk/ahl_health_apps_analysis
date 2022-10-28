a1f7cce3e7f7112041a22b1ad053ba8ddc4d13cd
imy99 <imanmuse@NESTA-FVFFP149Q05P.broadband>
2022-10-28 15:03:19 +0100

    adding preprocessing pipeline

---
+++ ahl_health_apps_analysis/pipeline/preprocessing.py
@@ -0,0 +1,26 @@
+from ahl_health_apps_analysis.utils.preprocessing_utils import *
+
+import pandas as pd
+
+details = pd.read_csv('inputs/data/app_ids_list_27-10.csv')
+
+
+details['description'] = details['descriptionHTML']
+details.drop(columns = 'descriptionHTML')
+
+# step 1 - replace with space
+details.dropna(subset = 'description', inplace= True)
+
+# step 2 - replace with space
+details['description'] = details['description'].apply(lambda x:replace_dash(x))
+
+# step 3 - remove emojis
+details['description'] = details['description'].apply(lambda x:remove_emojis(x))	                        
+
+#step 4 - replace html tags with space
+details['description'] = details['description'].apply(lambda x:remove_html_flags(x))
+
+
+
+details.to_csv('outputs/data/preprocessed_description_27-10.csv')
+
